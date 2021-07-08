646ebdea95220934211dd7b160d7b0efe68cbcbf
Your Name <you@example.com>
2021-07-08 16:20:06 -0700

    ran into compiling errors, so commented out entire makefile. i don't need fingerprint id anyway. specific errors: vendor/oss/fingerprint/egistec/current/BiometricsFingerprint.cpp:847:29: error: no matching constructor for initialization of 'const hidl_vec<uint8_t>' (aka 'const hidl_vec<unsigned char>')     const hidl_vec<uint8_t> token(hat_p, hat_p + sizeof(hw_auth_token_t));                             ^     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ system/libhidl/base/include/hidl/HidlSupport.h:292:5: note: candidate constructor not viable: requires single argument 'other', but 2 arguments were provided     hidl_vec(const hidl_vec<T> &other) : hidl_vec() {     ^ system/libhidl/base/include/hidl/HidlSupport.h:296:5: note: candidate constructor not viable: requires single argument 'other', but 2 arguments were provided     hidl_vec(hidl_vec<T> &&other) noexcept     ^ system/libhidl/base/include/hidl/HidlSupport.h:301:5: note: candidate constructor not viable: requires single argument 'list', but 2 arguments were provided     hidl_vec(const std::initializer_list<T> list)     ^ system/libhidl/base/include/hidl/HidlSupport.h:315:5: note: candidate constructor not viable: requires single argument 'other', but 2 arguments were provided     hidl_vec(const std::vector<T> &other) : hidl_vec() {     ^ system/libhidl/base/include/hidl/HidlSupport.h:285:5: note: candidate constructor not viable: requires 0 arguments, but 2 were provided     hidl_vec()
    error2:
    vendor/oss/fingerprint/egistec/legacy/EgisOperationLoops.cpp:158:29: error: no matching constructor for initialization of 'const hidl_vec<uint8_t>' (aka 'const hidl_vec<unsigned char>')
        const hidl_vec<uint8_t> token(hat_p, hat_p + sizeof(hw_auth_token_t));
                                ^     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    system/libhidl/base/include/hidl/HidlSupport.h:292:5: note: candidate constructor not viable: requires single argument 'other', but 2 arguments were provided
        hidl_vec(const hidl_vec<T> &other) : hidl_vec() {
        ^
    system/libhidl/base/include/hidl/HidlSupport.h:296:5: note: candidate constructor not viable: requires single argument 'other', but 2 arguments were provided
        hidl_vec(hidl_vec<T> &&other) noexcept
        ^
    system/libhidl/base/include/hidl/HidlSupport.h:301:5: note: candidate constructor not viable: requires single argument 'list', but 2 arguments were provided
        hidl_vec(const std::initializer_list<T> list)
        ^
    system/libhidl/base/include/hidl/HidlSupport.h:315:5: note: candidate constructor not viable: requires single argument 'other', but 2 arguments were provided
        hidl_vec(const std::vector<T> &other) : hidl_vec() {
        ^
    system/libhidl/base/include/hidl/HidlSupport.h:285:5: note: candidate constructor not viable: requires 0 arguments, but 2 were provided
        hidl_vec()

--- Android.mk
+++ Android.mk
@@ -1,85 +1,85 @@
-ifeq ($(PRODUCT_PLATFORM_SOD),true)
-ifneq ($(TARGET_DEVICE_NO_FPC), true)
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := android.hardware.biometrics.fingerprint@2.1-service.sony
-LOCAL_INIT_RC := android.hardware.biometrics.fingerprint@2.1-service.sony.rc
-LOCAL_PROPRIETARY_MODULE := true
-LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_SRC_FILES := \
-    $(call all-subdir-cpp-files) \
-    QSEEComFunc.c \
-    ion_buffer.c \
-    common.c
-
-ifeq ($(filter-out loire tone,$(SOMC_PLATFORM)),)
-LOCAL_SRC_FILES += fpc_imp_loire_tone.c
-HAS_FPC := true
-LOCAL_CFLAGS += -DHAS_DYNAMIC_POWER_MANAGEMENT
-endif
-
-ifeq ($(filter-out yoshino,$(SOMC_PLATFORM)),)
-LOCAL_SRC_FILES += fpc_imp_yoshino_nile_tama.c
-HAS_FPC := true
-LOCAL_CFLAGS += \
-    -DUSE_FPC_YOSHINO \
-    -DHAS_DYNAMIC_POWER_MANAGEMENT
-endif
-
-ifeq ($(filter-out nile,$(SOMC_PLATFORM)),)
-LOCAL_SRC_FILES += fpc_imp_yoshino_nile_tama.c
-HAS_FPC := true
-LOCAL_CFLAGS += \
-    -DUSE_FPC_NILE \
-    -DHAS_DYNAMIC_POWER_MANAGEMENT
-endif
-
-ifeq ($(filter-out tama,$(SOMC_PLATFORM)),)
-LOCAL_SRC_FILES += fpc_imp_yoshino_nile_tama.c
-HAS_FPC := true
-LOCAL_CFLAGS += \
-    -DUSE_FPC_TAMA \
-    -DHAS_DYNAMIC_POWER_MANAGEMENT
-endif
-
-ifeq ($(filter-out ganges,$(SOMC_PLATFORM)),)
-LOCAL_CFLAGS += \
-    -DUSE_FPC_GANGES \
-    -DHAS_DYNAMIC_POWER_MANAGEMENT
-endif
-
-ifeq ($(filter-out kumano,$(SOMC_PLATFORM)),)
-LOCAL_CFLAGS += -DUSE_FPC_KUMANO
-endif
-
-ifneq ($(HAS_FPC),true)
-# This file heavily depends on fpc_ implementations from the
-# above fpc_imp_* files. There is no sensible default file
-# on some platforms, so just remove the file altogether:
-LOCAL_SRC_FILES -= BiometricsFingerprint.cpp
-endif
-
-LOCAL_SHARED_LIBRARIES := \
-    android.hardware.biometrics.fingerprint@2.1 \
-    libcutils \
-    libdl \
-    libhardware \
-    libhidlbase \
-    libhidltransport \
-    libion \
-    liblog \
-    libutils
-
-ifeq ($(TARGET_COMPILE_WITH_MSM_KERNEL),true)
-LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
-LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
-endif
-
-LOCAL_CFLAGS += \
-    -DPLATFORM_SDK_VERSION=$(PLATFORM_SDK_VERSION) \
-    -fexceptions
-
-include $(BUILD_EXECUTABLE)
-endif
-endif # PRODUCT_PLATFORM_SOD
+#ifeq ($(PRODUCT_PLATFORM_SOD),true)
+#ifneq ($(TARGET_DEVICE_NO_FPC), true)
+#LOCAL_PATH := $(call my-dir)
+#
+#include $(CLEAR_VARS)
+#LOCAL_MODULE := android.hardware.biometrics.fingerprint@2.1-service.sony
+#LOCAL_INIT_RC := android.hardware.biometrics.fingerprint@2.1-service.sony.rc
+#LOCAL_PROPRIETARY_MODULE := true
+#LOCAL_MODULE_RELATIVE_PATH := hw
+#LOCAL_SRC_FILES := \
+#    $(call all-subdir-cpp-files) \
+#    QSEEComFunc.c \
+#    ion_buffer.c \
+#    common.c
+#
+#ifeq ($(filter-out loire tone,$(SOMC_PLATFORM)),)
+#LOCAL_SRC_FILES += fpc_imp_loire_tone.c
+#HAS_FPC := true
+#LOCAL_CFLAGS += -DHAS_DYNAMIC_POWER_MANAGEMENT
+#endif
+#
+#ifeq ($(filter-out yoshino,$(SOMC_PLATFORM)),)
+#LOCAL_SRC_FILES += fpc_imp_yoshino_nile_tama.c
+#HAS_FPC := true
+#LOCAL_CFLAGS += \
+#    -DUSE_FPC_YOSHINO \
+#    -DHAS_DYNAMIC_POWER_MANAGEMENT
+#endif
+#
+#ifeq ($(filter-out nile,$(SOMC_PLATFORM)),)
+#LOCAL_SRC_FILES += fpc_imp_yoshino_nile_tama.c
+#HAS_FPC := true
+#LOCAL_CFLAGS += \
+#    -DUSE_FPC_NILE \
+#    -DHAS_DYNAMIC_POWER_MANAGEMENT
+#endif
+#
+#ifeq ($(filter-out tama,$(SOMC_PLATFORM)),)
+#LOCAL_SRC_FILES += fpc_imp_yoshino_nile_tama.c
+#HAS_FPC := true
+#LOCAL_CFLAGS += \
+#    -DUSE_FPC_TAMA \
+#    -DHAS_DYNAMIC_POWER_MANAGEMENT
+#endif
+#
+#ifeq ($(filter-out ganges,$(SOMC_PLATFORM)),)
+#LOCAL_CFLAGS += \
+#    -DUSE_FPC_GANGES \
+#    -DHAS_DYNAMIC_POWER_MANAGEMENT
+#endif
+#
+#ifeq ($(filter-out kumano,$(SOMC_PLATFORM)),)
+#LOCAL_CFLAGS += -DUSE_FPC_KUMANO
+#endif
+#
+#ifneq ($(HAS_FPC),true)
+## This file heavily depends on fpc_ implementations from the
+## above fpc_imp_* files. There is no sensible default file
+## on some platforms, so just remove the file altogether:
+#LOCAL_SRC_FILES -= BiometricsFingerprint.cpp
+#endif
+#
+#LOCAL_SHARED_LIBRARIES := \
+#    android.hardware.biometrics.fingerprint@2.1 \
+#    libcutils \
+#    libdl \
+#    libhardware \
+#    libhidlbase \
+#    libhidltransport \
+#    libion \
+#    liblog \
+#    libutils
+#
+#ifeq ($(TARGET_COMPILE_WITH_MSM_KERNEL),true)
+#LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+#LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+#endif
+#
+#LOCAL_CFLAGS += \
+#    -DPLATFORM_SDK_VERSION=$(PLATFORM_SDK_VERSION) \
+#    -fexceptions
+#
+#include $(BUILD_EXECUTABLE)
+#endif
+#endif # PRODUCT_PLATFORM_SOD
